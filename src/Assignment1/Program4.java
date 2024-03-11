4243fb9f8ad7ad463f7507a7f8e9ca3f55872a77
Satheesh <pssatheesh2017@gmail.com>
2024-03-11 14:27:00 +0530

    updated Program4

--- src/Assignment1/Program4.java
+++ src/Assignment1/Program4.java
@@ -2,17 +2,24 @@ package Assignment1;
 
 import java.util.Scanner;
 class ClassA{
-
+    void classA1(){
+        System.out.println("hi");
+    }
 }
 class ClassB{
-
+    void classB1(){
+        System.out.println("hello");
+    }
 }
 class  ClassC{
-
+    void classC1(){
+        System.out.println("welcome");
+    }
 }
 public class Program4 {
 
     public static void main(String []args) {
-        System.out.println(Program4.class);
+
+        //System.out.println(Program4.class);
     }
 }
